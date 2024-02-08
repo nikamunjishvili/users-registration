92115003355a7c79432896f4e75d5cbbfd081c51
Nika munjisvili <nikamunja31415@gmail.com>
2024-02-08 16:30:43 +0400

    add commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Port } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const port = 3001;
-  await app.listen(port);
-  console.log(`Application started port: ${port}`);
+  const config = new DocumentBuilder()
+    .setTitle('Nest Project')
+    .setDescription('The users API registration')
+    .setVersion('1.0')
+    .addTag('Project')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(Port);
+  console.log(`Application started port: ${Port}`);
 }
 bootstrap();
